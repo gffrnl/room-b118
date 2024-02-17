3d184e6d39a0c4192b0efdb622c3afcfc0a0896c
Guilherme F. Fornel <gffrnl@gmail.com>
2024-02-17 00:08:13 -0300

    Change fast_symm_toeplitz_prod to reuse FFTW plan

--- modules/frlap/src/fstp.cpp
+++ modules/frlap/src/fstp.cpp
@@ -16,94 +16,116 @@ void fast_symm_toeplitz_prod(std::size_t const                      n,
                              double      const * const __restrict__ A1,
                              double      const * const __restrict__ x,
                              double            * const __restrict__ b) {
-  std::size_t k;  // padding
-  std::size_t m;  // the dimension of the augmented matrix
+    std::size_t k;  // padding
+    std::size_t m;  // the dimension of the augmented matrix
 
-  // Augmented arrays: A1 and x must be embedded into n+2*(k+1) arrays
-  double * mu;   // first row of augmented matrix
-  double * y;    // augmented vector
-  double * aux;  // auxiliary array
+    // Augmented arrays: A1 and x must be embedded into n+2*(k+1) arrays
+    double * mu;   // first row of augmented matrix
+    double * y;    // augmented vector
+    double * aux;  // auxiliary array
 
-  fftw_plan plan;
+    fftw_plan plan;
 
-  /* The sizes needed to allocate memory for the
-     augmented arrays */
-  k = (n % 2 == 0) ? (n - 4) / 2 : (n - 3) / 2;
-  m = n + 2 * (k + 1);
+    /* The sizes needed to allocate memory for the
+        augmented arrays */
+    k = (n % 2 == 0) ? (n - 4) / 2 : (n - 3) / 2;
+    m = n + 2 * (k + 1);
 
-  /* Augmented arrays allocation */
-  /*
-  if ((mu  = (double *) malloc(m * sizeof(double))) == NULL)
-      return 1;
-  if ((y   = (double *) malloc(m * sizeof(double))) == NULL)
-      return 2;
+    // https://www.fftw.org/fftw3_doc/
+    //
+    // 4.3.5 Real-to-Real Transforms
+    //
+    // ...
+    //
+    // fftw_plan fftw_plan_r2r_1d(int n, double *in, double *out,
+    //                            fftw_r2r_kind kind, unsigned flags);
+    //
+    // * n gives the (physical) size of the transform dimensions.
+    //   They can be any positive integer.
+    //
+    //   - FFTW is generally best at handling sizes of the form
+    //     2a 3b 5c 7d 11e 13f, where e+f is either 0 or 1, and the other
+    //     exponents are arbitrary. Other sizes are computed by means of a slow,
+    //     general-purpose algorithm (which nevertheless retains O(n log n)
+    //     performance even for prime sizes). (It is possible to customize FFTW
+    //     for different array sizes; see Installation and Customization.)
+    //     Transforms whose sizes are powers of 2 are especially fast.
+    //
+    //   - For a REDFT00 or RODFT00 transform kind in a dimension of size n,
+    //     it is n-1 or n+1, respectively, that should be factorizable in
+    //     the above form.
+    //
+    // ...
+    //
 
-  if ((aux = (double *) malloc(m * sizeof(double))) == NULL)
-      return 3;
-  */
+    mu  = static_cast<double *>(fftw_malloc(sizeof(double) * m));
+    y   = static_cast<double *>(fftw_malloc(sizeof(double) * m));
+    aux = static_cast<double *>(fftw_malloc(sizeof(double) * m));
 
-  if ((mu  = (double *) calloc(m, sizeof(double))) == NULL)
-      abort();
-  if ((y   = (double *) calloc(m,  sizeof(double))) == NULL)
-      abort();
+    // // TODO(gffrnl): replace for AFTER memsets
+    // std::memset((void *)  mu, 0, m * sizeof(double));
+    // std::memset((void *)   y, 0, m * sizeof(double));
+    std::memset(static_cast<void *>(mu),
+                0,
+                (m - n) * sizeof(double));
+    std::memset(static_cast<void *>(y),
+                0,
+                m * sizeof(double));
+    std::memset(static_cast<void *>(aux),
+                0,
+                m * sizeof(double));
 
-  if ((aux = (double *) calloc(m, sizeof(double))) == NULL)
-      abort();
+    // Construct of the the augmented arrays
+    std::memcpy(static_cast<void *>(mu),    static_cast<void const *>(A1),
+                n * sizeof(double));
+    for (std::size_t i = 2; i < n; ++i)
+        mu[i-2] -= A1[i];
+    std::memcpy(static_cast<void *>(y + k + 1), static_cast<void const *>(x),
+                n * sizeof(double));
 
+    // TODO(gffrnl): replacement for the BEFORE memsets
+    std::memset(static_cast<void *>(mu + n),
+                0,
+                (m - n) * sizeof(double));
+    std::memset(static_cast<void *>(y),
+                0,
+                (k + 1) * sizeof(double));
+    std::memset(static_cast<void *>(y + k + 1 + n),
+                0,
+                (k + 1) * sizeof(double));
 
-  /* Construct of the the augmented arrays */
-  /*
-  memset((void *) (mu+n)   , 0, (m-n) * sizeof(double));
-  memset((void *)  y       , 0, (k+1) * sizeof(double));
-  memset((void *) (x+k+n+1), 0, (k+1) * sizeof(double));
-  // TODO: INSTEAD THE ABOVE, TRY CALLOC
-  // WARNING: THESE memsets are wrong !!!
-  */
+    // Compute the DST1 of mu and store in aux
+    plan = fftw_plan_r2r_1d(m, mu, aux, FFTW_RODFT00, FFTW_ESTIMATE);
+    fftw_execute_r2r(plan, mu, aux);
 
-  std::memcpy(static_cast<void *>(mu),    static_cast<void const *>(A1),
-              n * sizeof(double));
-  for (size_t i = 2; i < n; ++i)
-      mu[i-2] -= A1[i];
-  std::memcpy(static_cast<void *>(y+k+1), static_cast<void const *>(x),
-              n * sizeof(double));
+    // Compute the DST1 of y and store in mu
+    fftw_execute_r2r(plan, y, mu);
 
-  // Compute the DST1 of mu and store in aux
-  plan = fftw_plan_r2r_1d(m, mu, aux, FFTW_RODFT00, FFTW_ESTIMATE);
-  fftw_execute(plan);
-  fftw_destroy_plan(plan);
-  // fftw_cleanup();
+    fftw_free(y);
 
-  // Compute the DST1 of y and store in mu
-  plan = fftw_plan_r2r_1d(m, y, mu, FFTW_RODFT00, FFTW_ESTIMATE);
-  fftw_execute(plan);
-  fftw_destroy_plan(plan);
-  // fftw_cleanup();
+    // Multiply
+    {
+        double const scaling = 1.0 / (4.0 * (m + 1));
+        double const delta_theta = M_PI / (m + 1);
+        for (std::size_t i = 0; i < m; ++i)
+            aux[i] *= (scaling * mu[i] / sin((i + 1) * delta_theta));
+    }
+    // NOTE: TODO(grffrnl) REWRITE THIS NOTE
+    // need to multiply lamb by 1/2 rather
+    //          sqrt(n+1)/2 because in fftw3 scaling
+    //          of DST1 is 2
 
-  std::free(y);  // y no more needed
+    // Now compute the DST1 of aux
+    fftw_execute_r2r(plan, aux, mu);
+    fftw_destroy_plan(plan);
 
-  // Multiply
-  {
-    double const scaling = 1.0 / (4.0 * (m + 1));
-    double const delta_theta = M_PI / (m + 1);
-    for (std::size_t i = 0; i < m; ++i)
-        aux[i] *= (scaling * mu[i] / sin((i + 1) * delta_theta));
-  }
-  // NOTE: TODO(grffrnl) REWRITE THIS NOTE
-  // need to multiply lamb by 1/2 rather
-  //          sqrt(n+1)/2 because in fftw3 scaling
-  //          of DST1 is 2
+    fftw_free(aux);
 
-  /* Now compute the DST1 of xemb */
-  plan = fftw_plan_r2r_1d(m, aux, mu, FFTW_RODFT00, FFTW_ESTIMATE);
-  fftw_execute(plan);
-  fftw_destroy_plan(plan);
-  fftw_cleanup();
+    // Copy only necessary values to b
+    std::memcpy(static_cast<void *>(b), static_cast<void const *>(mu + k + 1),
+                n * sizeof(double));
 
-  std::free(aux);  // aux no more needed
+    fftw_free(mu);
 
-  // Copy only necessary values to b
-  std::memcpy(static_cast<void *>(b), static_cast<void const *>(mu+k+1),
-              n * sizeof(double));
-
-  std::free(mu);
+    fftw_cleanup();
 }
