e68a3345270de5ac1f3784a16a76e2d6b3418823
Fabio <fazedo@gmail.com>
2024-02-19 11:00:52 -0300

    Fiz as simplificações

--- modules/frlap/src/gdm/strategies/huang_oberman_quadratic.cpp
+++ modules/frlap/src/gdm/strategies/huang_oberman_quadratic.cpp
@@ -32,29 +32,31 @@
 #define M_1_SQRTPI 0.564189583547756286948079451561
 #endif
 
+// The following two functions are involved in calculating the coefficients
+// The formulation via Taylor Series is regular at alpha = 1 and removes
+// cathastrophic cancelations near both alpha = 1 and k -> infinity.
+// In both the first term is summed in the end, so we have a bit of increased accuracy.
+// Both present the asymptotics 4/6 as k -> infinity
 template <class Real>
 Real serie_par(int k, Real alpha) {
     Real inv_k = 1/static_cast<Real>(k);
     Real x = inv_k*inv_k;
     Real x_2n = 1;
     Real prod = 1/static_cast<Real>(6);  // (alpha-1)*alpha/(n+1)! para n=2.
-    Real T0 = 2*prod;
-    Real soma = 0;  // 2*prod;
+    Real T0 = 2*prod;  // first term of the series
+    Real soma = 0;
 
     for (int n = 4 ; n < 300; n += 2) {
-        prod *= (alpha-3 + n)*(alpha-2 + n)/(n*(n+1));
+        prod *= (alpha - 3 + n)*(alpha - 2 + n)/(n*(n+1));
         x_2n *= x;
-        auto const soma_old = soma;
+        auto soma_old = soma;
         soma = soma_old + prod*x_2n*n;
         if (soma == soma_old) {
             break;
         }
     }
-    // return 2 * x * (T0 + soma) * pow(k, 1 - alpha);  // /(alpha*(alpha-1));
     return 2 * (T0 + soma);
-}
-
-
+}  // Asymptotics = 4/6 as k -> infinity
 
 template <class Real>
 Real serie_impar(int k, Real alpha) {
@@ -67,25 +69,27 @@ Real serie_impar(int k, Real alpha) {
     for (int n = 4 ; n < 300; n += 2) {
         prod *= (alpha-3 + n)*(alpha-2 + n)/(n*(n+1));
         x_2n *= x;
-        auto const soma_old = soma;
+        auto soma_old = soma;
         soma = soma_old + prod*x_2n*(3-n);
         if (soma == soma_old) {
             break;
         }
     }
-    // return x * (soma + T0) * pow(k, 1 - alpha);  // /(alpha*(alpha-1));
     return (soma + T0)*4;
+}  // Asymptotics = 4/6 as k -> infinity
 
-}
-
-
+// This function calcultes the coefficient para k = 1 for ealpha != 1.
+// The limit for ealpha -> 1 is (8 - 5*std::log(3))/2,
+// which comes from:
+// (exp(a*x) - 1)/x = a,   x -> 0.
 template <class Real>
 Real melhorada_1(Real alpha) {  // k = 1 alpha != 1
-    Real log3 = log(3);
+    Real log3 = std::log(3);
     Real T_1 = -std::expm1(log3*(1 - alpha)) / (1 - alpha);
     Real T_2 = 4 + (alpha + 4) * T_1 / 2;
     return T_2 / (alpha * (2 - alpha));
 }
+
 extern double d0G_alpha_ne_1(double, std::size_t);
 extern double d1G_alpha_ne_1(double, std::size_t);
 extern double d2G_alpha_ne_1(double, std::size_t);
@@ -115,18 +119,17 @@ void huang_oberman_quadratic::generate_coefficients(double      ealpha,
         }
     }
 
+    bool const ealpha_one = b118::almost_equal(ealpha, 1.0);
+
     {  // {0 < ealpha < 2}
-        double ca = 0.0;
-        double ch = 0.0;
 
-        if (b118::almost_equal(ealpha, 1.0)) {
-            ca = - frlap::normal_const<1>(1.0);
-            ch = 1.0 / deltax;
-        } else {
-            ca = - frlap::normal_const<1>(ealpha);  // -C1,alpha
-            ch = std::pow(deltax, -ealpha);         // 1/h^alpha
-        }
+    // Now we treat the general case 0 < ealpha < 2
+        bool const ealpha_one = b118::almost_equal(ealpha, 1.0);
+        double const ca = ealpha_one ? - frlap::normal_const<1>(1.0)
+                                     : - frlap::normal_const<1>(ealpha);
 
+        double const ch = ealpha_one ? + 1.0 / deltax
+                                     : + std::pow(deltax, -ealpha);
 
         // REMARK: using Log-Gamma function and then
         //         exponentiate seems better to mitigate
@@ -137,38 +140,23 @@ void huang_oberman_quadratic::generate_coefficients(double      ealpha,
         //         our Gammas are always positive.
         //
         coeffs[0] = ch * M_1_SQRTPI * std::exp2(ealpha) *
-            std::exp(std::lgamma(0.5 * (ealpha + 1.0))
-                - std::lgamma(2.0 -  0.5 * ealpha));
+            std::exp(std::lgamma(0.5 + 0.5 * ealpha)
+                   - std::lgamma(2.0 - 0.5 * ealpha));
 
-        auto ca_ch = ca*ch;
-        if (b118::almost_equal(ealpha, 1.0)) {
-            coeffs[1] = ca_ch * (1.0
-                -       d2G_alpha_eq_1(1)
-                - 0.5 * d1G_alpha_eq_1(3)
-                - 1.5 * d1G_alpha_eq_1(1)
-                +       d0G_alpha_eq_1(3)
-                -       d0G_alpha_eq_1(1));
-
-            for (std::size_t k = 2; k < n; k+=2)
-                coeffs[k] = ca_ch * 2.0 * (d1G_alpha_eq_1(k+1)
-                        + d1G_alpha_eq_1(k-1)
-                                        - d0G_alpha_eq_1(k+1)
-                                        + d0G_alpha_eq_1(k-1));
-
-            for (std::size_t k = 3; k < n; k+=2)
-                coeffs[k] = ca_ch * (- 0.5 * (d1G_alpha_eq_1(k+2)
-                            + d1G_alpha_eq_1(k-2))
-                        - 3.0 * d1G_alpha_eq_1(k)
-                        +       d0G_alpha_eq_1(k+2)
-                        -       d0G_alpha_eq_1(k-2));
+        // Obs: coeffs[1] is continuous at ealpha = 1 but we prefered to treat it separately.
+    
+        if (ealpha_one == true) {  // Could test only ealpha == 1
+            coeffs[1] = ca*ch * (8 - 5*std::log(3))/2;
         } else {
-            coeffs[1] = ca_ch * melhorada_1(ealpha);
-
-            for (std::size_t k = 2; k < n; k+=2)
-                coeffs[k] = ca * 2.0 * serie_par(k, ealpha)*std::pow(deltax * k, - ealpha)/k;
-
-            for (std::size_t k = 3; k < n; k+=2)
-                coeffs[k] = ca * serie_impar(k, ealpha)*std::pow(deltax * k, - ealpha)/k;
+            coeffs[1] = ca*ch * melhorada_1(ealpha);
         }
+
+        // The formulation in Taylor series allows us to use
+        // the same loop for both even and odd indices.
+        for (std::size_t k = 2; k < n; k+=2)
+            coeffs[k] = ca * 2.0 * serie_par(k, ealpha)*std::pow(deltax * k, - ealpha)/k;
+
+        for (std::size_t k = 3; k < n; k+=2)
+            coeffs[k] = ca * serie_impar(k, ealpha)*std::pow(deltax * k, - ealpha)/k;
     }
 }
